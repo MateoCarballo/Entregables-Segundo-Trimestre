c323d36f6efc41a391ac428ed86489c60b29f7a8
Mateo <mateocarballo2@gmail.com>
2023-02-20 23:13:00 +0100

    .

--- Ejercicio1/src/Enumeracion.java
+++ Ejercicio1/src/Enumeracion.java
@@ -1,22 +1,22 @@
 public class Enumeracion {
 
-    final String [] marcas = {"Toyota","Ford","Honda","Chevrolet","Nissan","Audi","BMW","Mercedes-Benz","Porsche","Ferrari","Maserati","Jaguar","Land Rover","Bentley",};
+    final String [] marcasCoche = {"Toyota","Ford","Honda","Chevrolet","Nissan","Audi","BMW","Mercedes-Benz","Porsche","Ferrari","Maserati","Jaguar","Land Rover","Bentley",};
 
-    final String[][] modelos = {  {"Corolla", "Camry", "RAV4", "Prius"},
-                                {"Mustang", "Explorer", "F-150", "Focus"},
-                                {"Civic", "Accord", "CR-V", "Pilot"},
-                                {"Camaro", "Silverado", "Impala", "Equinox"},
-                                {"Altima", "Maxima", "Rogue", "Pathfinder"},
-                                {"A3", "A4", "Q5", "TT"},
-                                {"3 Series", "5 Series", "X3", "X5"},
-                                {"C-Class", "E-Class", "GLC", "S-Class"},
-                                {"911", "Cayenne", "Panamera", "Macan"},
-                                {"Lamborghini", "Aventador", "Huracan", "Urus", "Sian"},
-                                {"458 Italia", "488 GTB", "Portofino", "812 Superfast"},
-                                {"Ghibli", "Quattroporte", "Levante", "GranTurismo"},
-                                {"F-Type", "XE", "XF", "F-PACE"},
-                                {"Range Rover Sport", "Range Rover Velar", "Discovery Sport", "Defender"},
-                                {"Continental GT", "Flying Spur", "Bentayga", "Mulsanne"}
+    final String[][] modelosCoche = {  {"Corolla", "Camry", "RAV4", "Prius"},
+            {"Mustang", "Explorer", "F-150", "Focus"},
+            {"Civic", "Accord", "CR-V", "Pilot"},
+            {"Camaro", "Silverado", "Impala", "Equinox"},
+            {"Altima", "Maxima", "Rogue", "Pathfinder"},
+            {"A3", "A4", "Q5", "TT"},
+            {"3 Series", "5 Series", "X3", "X5"},
+            {"C-Class", "E-Class", "GLC", "S-Class"},
+            {"911", "Cayenne", "Panamera", "Macan"},
+            {"Lamborghini", "Aventador", "Huracan", "Urus", "Sian"},
+            {"458 Italia", "488 GTB", "Portofino", "812 Superfast"},
+            {"Ghibli", "Quattroporte", "Levante", "GranTurismo"},
+            {"F-Type", "XE", "XF", "F-PACE"},
+            {"Range Rover Sport", "Range Rover Velar", "Discovery Sport", "Defender"},
+            {"Continental GT", "Flying Spur", "Bentayga", "Mulsanne"}
     };
 
 
