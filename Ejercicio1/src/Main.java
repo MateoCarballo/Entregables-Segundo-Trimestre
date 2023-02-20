b1cb966ac840f57137bcaf48dc80c44370b94bd1
Mateo <mateocarballo2@gmail.com>
2023-02-20 22:59:59 +0100

    creado menu en main

--- Ejercicio1/src/Main.java
+++ Ejercicio1/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        boolean continuar = true;
+
+        do {
+            System.out.println("""
+                         GESTION CONSCESIONARIO
+                         
+                    1.VER VEHICULOS.
+                    2.AÑADIR VEHICULO.
+                    3.VENDER VEHICULO.
+                    4.VER ESTADISTICAS DE UN VEHICULO.
+                    5.SALIR.                          
+                    """);
+                    /*
+                    Pendiente introducir los metodos que vendran dados por la clase empresa
+                     */
+        }while(continuar);
+
     }
 }
