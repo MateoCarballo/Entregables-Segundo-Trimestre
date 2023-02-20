0b5fd79edf289a0529e90f676e92a296bb2b8135
Mateo <mateocarballo2@gmail.com>
2023-02-20 23:51:55 +0100

    Planteando metodos en concesionario, paso previo a merge

--- Ejercicio1/src/Concesionario.java
+++ Ejercicio1/src/Concesionario.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Concesionario extends Empresa{
     int totaldeVehiculos;
     int totalMotos;
@@ -6,9 +8,36 @@ public class Concesionario extends Empresa{
     int totalCamiones;
     int facturacion;
 
+    public static final ArrayList <Vehiculo> vehiculosConcesionario =new ArrayList<Vehiculo>();
     public Concesionario(String nombreEmpresa, String cif, String cuentaSS, String url, String razonSocial, String direccionPostal, String numeroTelefono1, String emailcorporativo1, String ip1) {
         super(nombreEmpresa, cif, cuentaSS, url, razonSocial, direccionPostal, numeroTelefono1, emailcorporativo1, ip1);
     }
 
+    public static void añadirMoto(){
 
+    }
+    public static void añadirCoche(){
+
+    }
+    public static void añadirFurgoneta(){
+
+    }
+    public static void añadirCamion(){
+
+    }
+    public static void verVehiculos(){
+
+    }
+    public static void verMotos(){
+
+    }
+    public static void verCoches(){
+
+    }
+    public static void verFurgonetas(){
+
+    }
+    public static void verCamiones(){
+
+    }
 }
