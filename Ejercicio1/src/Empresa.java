056b5949ca32b6a80b158e9989f7dfd7eb69e80c
Mateo <mateocarballo2@gmail.com>
2023-02-20 23:22:32 +0100

    Creada clase padre Empresa

---
+++ Ejercicio1/src/Empresa.java
@@ -0,0 +1,21 @@
+public class Empresa {
+    private String cif;
+    private String cuentaSS;
+    private String url;
+    private String razonSocial;
+    private String direccionPostal;
+    private String numeroTelefono1;
+    private String emailcorporativo1;
+    private String ip1;
+
+    public Empresa(String cif, String cuentaSS, String url, String razonSocial, String direccionPostal, String numeroTelefono1, String emailcorporativo1, String ip1) {
+        this.cif = cif;
+        this.cuentaSS = cuentaSS;
+        this.url = url;
+        this.razonSocial = razonSocial;
+        this.direccionPostal = direccionPostal;
+        this.numeroTelefono1 = numeroTelefono1;
+        this.emailcorporativo1 = emailcorporativo1;
+        this.ip1 = ip1;
+    }
+}
