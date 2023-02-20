298129e0a6f0109b6ab60a084f58ffabcf820b59
Mateo <mateocarballo2@gmail.com>
2023-02-20 23:33:51 +0100

    .

--- Ejercicio1/src/Concesionario.java
+++ Ejercicio1/src/Concesionario.java
@@ -1,10 +1,14 @@
 public class Concesionario extends Empresa{
     int totaldeVehiculos;
+    int totalMotos;
+    int totalCoches;
+    int totalFurgonetas;
+    int totalCamiones;
     int facturacion;
 
-    public Concesionario(String nombreEmpresa, String cif, String cuentaSS, String url, String razonSocial, String direccionPostal, String numeroTelefono1, String emailcorporativo1, String ip1, int totaldeVehiculos, int facturacion) {
+    public Concesionario(String nombreEmpresa, String cif, String cuentaSS, String url, String razonSocial, String direccionPostal, String numeroTelefono1, String emailcorporativo1, String ip1) {
         super(nombreEmpresa, cif, cuentaSS, url, razonSocial, direccionPostal, numeroTelefono1, emailcorporativo1, ip1);
-        this.totaldeVehiculos = totaldeVehiculos;
-        this.facturacion = facturacion;
     }
+
+
 }
